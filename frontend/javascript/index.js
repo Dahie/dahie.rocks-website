172b4bd48c7c9b83bcdfa545e434a03356bad799
Daniel Senff <mail@danielsenff.de>
2021-11-13 11:42:33 +0100

    update bridgetown to 0.21.5

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -4,10 +4,9 @@ import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
 
-
-
 // Import all javascript files from src/_components
 const componentsContext = require.context("bridgetownComponents", true, /.js$/)
 componentsContext.keys().forEach(componentsContext)
 
 console.info("Bridgetown is loaded!")
+
